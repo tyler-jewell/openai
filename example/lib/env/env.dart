4212bf0f43588ab60908bceb88583708e80973c7
tyler.p.jewell <tyler.p.jewell@gmail.com>
2024-10-15 17:13:55 +0000

    remove path=".env"

--- example/lib/env/env.dart
+++ example/lib/env/env.dart
@@ -3,7 +3,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: ".env")
+@Envied()
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY')
   static const String apiKey = _Env.apiKey;
